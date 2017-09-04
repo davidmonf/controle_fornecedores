a96b1fed8475dac8e62dc741fa2352c04d15f04f
Caique Santos <caiquebsan@gmail.com>
2017-09-04 19:04:35 -0300

    Primeito commit

---
+++ input_simpress.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 79240SP
+ * Date: 04/09/2017
+ * Time: 18:21
+ */
+?>
+
+<html>
+<head>
+
+</head>
+
+<body>
+<form>
+    <label>Série: <input type="text" name="serie"><br></label>
+    <label>IP: <input type="text" name="ip"><br>
+    <label>Item Contrato: <input type="text" name="item_contrato"><br>
+    <label>Modelo: <input type="text" name="modelo"><br>
+    <label>Instalação: <input type="text" name="instalacao"><br>
+    Desinstalação: <input type="text" name="desinstalacao"><br>
+    Valor: <input type="text" name="valor"><br>
+    Código CR: <input type="text" name="cr"><br>
+    Descrição CR: <input type="text" name="descricao_cr"><br>
+    Superintendência: <input type="text" name="superint"><br>
+    Gerência: <input type="text" name="gerencia"><br>
+    Endereço: <input type="text" name="endereco"><br>
+    Cidade: <input type="text" name="cidade"><br>
+    UF: <input type="text" name="uf"><br>
+    CEP: <input type="text" name="cep"><br>
+</form>
+</body>
+
+</html>
