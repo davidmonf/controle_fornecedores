8e5b54e0e5f0ff04b24452de45ede000738939fc
Caique Santos <caiquebsan@gmail.com>
2017-09-05 18:27:04 -0300

    Remoção do teste navbar

--- input_simpress.php
+++ input_simpress.php
@@ -20,30 +20,7 @@
 <body>
     <script src="src/js/jquery-3.2.1.min.js"></script>
     <script src="src/js/bootstrap.min.js"></script>
-
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Controle Fornecedores</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Teste</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <div id="main" class="container-fluid">
-        <h3 class="page-header">Teste</h3>
-    </div>
-
+    
     <form>
         <label>Série: <input type="text" name="serie"><br></label>
         <label>IP: <input type="text" name="ip"><br>
